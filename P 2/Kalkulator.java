97d54139b1caab36418dd2b75fe546b8e1d0c86e
abdulaziz1999 <azizmentor96@gmail.com>
2020-11-13 18:32:23 +0700

    Java PBO

--- P 2/Kalkulator.java
+++ P 2/Kalkulator.java
@@ -10,7 +10,7 @@ class Kalkulator{
     }
 
     public static void perkalian(int nil1, int nil2){
-        int hasil = nil1 * nil2; 
+        int hasil = nil1 * nil2;
         System.out.print(hasil);
     }
 }
